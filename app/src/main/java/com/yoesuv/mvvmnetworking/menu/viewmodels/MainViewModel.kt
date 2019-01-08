bd59b36bdaed848a2e890bf51eef29a704da2688
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-01-08 16:24:31 +0700

    :pencil: connect API

--- app/src/main/java/com/yoesuv/mvvmnetworking/menu/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/yoesuv/mvvmnetworking/menu/viewmodels/MainViewModel.kt
@@ -2,6 +2,28 @@ package com.yoesuv.mvvmnetworking.menu.viewmodels
 
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.MutableLiveData
+import com.yoesuv.mvvmnetworking.menu.models.ListPlaceResponseModel
+import com.yoesuv.mvvmnetworking.networks.MainRepository
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository = MainRepository()
+
+    var listPlace: MutableLiveData<ListPlaceResponseModel> = MutableLiveData()
+    var error: MutableLiveData<Throwable> = MutableLiveData()
+
+    fun getListPlace(){
+        repository.requestListPlace({
+            listPlace.postValue(it)
+        },{
+            error.postValue(it)
+        })
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        repository.onDestroy()
+    }
+
 }
--- app/src/main/java/com/yoesuv/mvvmnetworking/menu/views/MainActivity.kt
+++ app/src/main/java/com/yoesuv/mvvmnetworking/menu/views/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.yoesuv.mvvmnetworking.menu.views
 
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.yoesuv.mvvmnetworking.R
+import com.yoesuv.mvvmnetworking.databinding.ActivityMainBinding
+import com.yoesuv.mvvmnetworking.menu.viewmodels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        binding.main = viewModel
+
+        viewModel.getListPlace()
     }
 }
